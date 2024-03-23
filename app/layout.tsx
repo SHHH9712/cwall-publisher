bbd1f62c1643c9f65f019e9076e96f7512016f9b
sha <shhh9712@gmail.com>
2024-03-23 01:30:43 -0700

    fix clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider publishableKey={process.env.CLERK_SECRET_KEY}>
       <html lang="en">
         <body className={inter.className}>
           <ThemeProvider
