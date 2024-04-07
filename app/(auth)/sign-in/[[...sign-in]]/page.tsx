f4346e13b182f63a28281834a27ee9077a2938e9
sha <shhh9712@gmail.com>
2024-04-06 19:31:38 -0700

    improve

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,17 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="flex flex-col md:flex-row h-full text-left items-center justify-evenly overflow-hidden">
-      <div className="text-5xl">
-        <h1>Build your Instagram account using: </h1>
-        <h1>
-          <span className="underline">cwall-publisher</span>
-        </h1>
-      </div>
-      <div className="origin-center scale-90 md:scale-100">
-        <SignIn />
-      </div>
-    </div>
-  );
+  return <SignIn />;
 }
